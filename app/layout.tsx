d1c95e8dcc108a8f58585a8d85028773d1c447b8
sofman65 <sofman65@gmail.com>
2024-08-21 19:56:08 +0300

    Deployment attempt

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import { BackgroundGradientAnimation } from "@/app/components/ui/background-gradient-animation";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
